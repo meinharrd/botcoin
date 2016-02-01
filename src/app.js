3c176ce0f92af953bc54fb17dd52df50bbd9ef5b
Thiago Fassina <thiagoaaa@gmail.com>
2016-02-01 01:20:34 -0200

    added a bunch of dependencies

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
+var config = require('./config.json');
+
+// firebase
+var Firebase = require('firebase');
+var firebaseRef = new Firebase(config.firebase);
+
+
+// api loop
+var request = require('request');
+request(config.dataApi+'orderbook', function(error, response, body) {
+	firebaseRef.set(JSON.parse(body));
+});
+
+
+// webserver
 var express = require('express');
 var jade = require('jade');
 
 var app = express();
+
+app.use('/app', express.static(__dirname+'/dashboard/app'));
 
 app.get('/', function(req, res) {
 	res.send(jade.renderFile(__dirname+'/dashboard/main.jade'));
