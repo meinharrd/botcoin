ace960488ca5715ab7a902792f08d153dc0aba69
Thiago Fassina <thiagoaaa@gmail.com>
2016-02-02 00:59:07 -0200

    angular + firebase hello world

--- src/dashboard/app/main.js
+++ src/dashboard/app/main.js
@@ -1,1 +1,6 @@
-mainApp = angular.module('main', []);
+mainApp = angular.module('main', ['firebase']);
+
+mainApp.controller('mainCtrl', function($scope, $firebaseObject) {
+	var ref = new Firebase('your firebase url');
+	$scope.data = $firebaseObject(ref);
+});
